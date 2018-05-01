8597e974045552ef28ebae265d88082002305ea2
slogothe <whateverxequals@gmail.com>
2018-05-01 10:41:05 -0400

    Added comments to app.R; added functions to rough.R to save data in CSV format

--- app.R
+++ app.R
@@ -1,10 +1,14 @@
+# Main shiny interface
+
 library(shiny)
 library(tidyverse)
 library(rlang)
 library(knitr)
-source("rough.R")
+source("rough.R") # Functions that are sourced
+
 ui <- fluidPage(tabsetPanel(type = "tabs", tabPanel("Game and Items Data",
   sidebarLayout(
+    # Sets inputs from the side panel, like game name, plot type, and table settings
     sidebarPanel(width = 2,
   textInput("game", "Enter Game Here", "Team Fortress 2"),
   selectInput(inputId = "plot_type",label = "Type of Plot:", choices = c("Scatterplot", "Bar Chart")),
@@ -13,10 +17,12 @@ ui <- fluidPage(tabsetPanel(type = "tabs", tabPanel("Game and Items Data",
   numericInput("numrows", "Number of Rows for Table to Display", 20, min = 1, max = 40),
   selectInput("arrangeby", "Arrange Table in order of", choices = c("Price" = "results.sell_price", "Number of Items for Sale" = "results.sell_listings"))
     ),
+  # Outputs plot and table in a dynmamic setting
   mainPanel(
         fluidRow(column(8,plotOutput("plot1",  height = 800)),
                   column(4, tableOutput('table1'))))
             )),
+  # Tab for stock data and it's associated sidebar options
                 tabPanel("Stock Data", sidebarLayout(
                   sidebarPanel(width = 2, textInput("symbol", "Enter Stock Symbol Here", "MSFT"),
                                radioButtons("date_range", "Range of dates to plot",
@@ -29,9 +35,11 @@ ui <- fluidPage(tabsetPanel(type = "tabs", tabPanel("Game and Items Data",
 ))
 
 server <- function(input, output){
+  # CHecks various settings for the Scatterplot of the game's items. The first if statement checks to see what type of plot is specified
   output$plot1 <- renderPlot({
     if(input$plot_type == "Scatterplot"){
-      if(input$logtransform){
+      if(input$logtransform){ 
+        # Checks for log transform
         get_items_and_prices(input$game) %>% ggplot(aes(results.sell_price, log(results.sell_listings))) + geom_point() + xlim(input$range_price) + xlab("Price of Item (cents)")+
           ylab("Number of Items for Sale") + ggtitle(paste("Log Plot of Items For Sale in", input$game)) + theme(plot.title = element_text(size = 30)) + 
           theme(axis.title = element_text(size = 20)) + theme(axis.text = element_text(size = 15))
@@ -42,9 +50,10 @@ server <- function(input, output){
           theme(axis.title = element_text(size = 20))+ theme(axis.text = element_text(size = 15))
       }
 
-    }
+    } # Same as above, but for a bar graph representaiton. Note the summarise command to get the total number of items for sale at a specific price
     else if(input$plot_type == "Bar Chart"){
       if(input$logtransform){
+        # Checks for log transform
       get_items_and_prices(input$game) %>% group_by(results.sell_price) %>%  summarise(n = sum(results.sell_listings)) %>% ggplot(aes(results.sell_price, log(n))) + 
         geom_bar(stat = "identity")+ xlim(input$range_price) + xlab("Price of Item (cents)") + ylab("Log(Number of Items for Sale)") + 
           ggtitle(paste("Log Plot of Items For Sale in", input$game)) + theme(plot.title = element_text(size = 30)) + theme(axis.title = element_text(size = 20)) + 
@@ -58,9 +67,11 @@ server <- function(input, output){
       }
     }
   })
+  # Table output for list of items for sale
   output$table1 <- renderTable({get_items_and_prices(input$game) %>% arrange_(input$arrangeby) %>% .[1:input$numrows,] %>%  
       subset(select = -c(results.app_icon, results.sell_price, results.app_name)) %>% 
       plyr::rename(replace = c("results.name" = "Name of Item", "results.sell_listings" = "Number for sale", "results.sell_price_text" = "Price"))})
+  # Plot output for stock data
   output$plot2 <- renderPlot({get_financial_info(input$symbol, days_to_plot = input$date_range) %>% na.omit() %>% ggplot(aes(Date, Open)) + geom_line() + 
       xlab("Time (days)") + ylab("Price of Stock") + ggtitle(paste("Value of Stock over the past", input$date_range, "Days")) + theme(plot.title = element_text(size = 30)) +
       theme(axis.title = element_text(size = 20)) + theme(axis.text = element_text(size = 15))})
--- rough.R
+++ rough.R
@@ -1,20 +1,24 @@
-# Steam scraper
+# Steam scraper functions
 library(httr)
 library(rvest)
 library(curl)
 library(tseries)
 library(jsonlite)
 library(tidyverse)
+
+# Gets list of games and their associated steamID's 
 game_list <- "http://api.steampowered.com/ISteamApps/GetAppList/v0002/"%>% curl() %>% readLines() %>% fromJSON(flatten = TRUE) %>% as.data.frame()
 
+# Funciton that gets list of items for sale, based on the Steam Community API
 get_items_and_prices <- function(app.name, num_items = 100){
+  # Check to see if game specified is in Steam's library. If not, return error
   if(app.name %in% game_list$applist.apps.name){
     appid = game_list$applist.apps.appid[which(game_list$applist.apps.name == app.name)]
   }
   else{
     stop("Your game isn't in Steam's Library. Check your spelling or try another game")
   }
-  
+  # Since Steam only allows 100 queries at a time, this for loop allows the user to get more than 100 items for a specific game, should they choose
   for(i in 0:(num_items/100 - 1)){
     url = paste("https://steamcommunity.com/market/search/render/?query=&start=", 100*i, "&count=100&norender=1&appid=", appid, sep = "")
     new_data = url %>% curl() %>% readLines() %>% fromJSON(flatten = TRUE) %>% as.data.frame() %>%  subset(select = c("results.name", "results.sell_listings","results.sell_price", "results.sell_price_text", "results.app_icon","results.app_name"))
@@ -28,22 +32,40 @@ get_items_and_prices <- function(app.name, num_items = 100){
   out
 }
 
+# An unused, but helpful function that gets the name of the publisher based on the name of the game
 get_game_info <- function(app.name){
+  # Check to see if game specified is in Steam's library. If not, return error
   if(app.name %in% game_list$applist.apps.name){
     appid = game_list$applist.apps.appid[which(game_list$applist.apps.name == app.name)]
   }
   else{
     stop("Your game isn't in Steam's Library. Check your spelling or try another game")
   }
+  # Uses Steam's store API to get the publisher name
   url = paste("https://store.steampowered.com/api/appdetails?appids=", appid, sep = "")
   out = url %>% curl() %>% readLines() %>% fromJSON(flatten = TRUE) %>% as_data_frame() 
   names(out) <- "V1"
   out$V1$data$publisher
 }
 
+# Using the get.hist.quote function from the tseries library, outputs a time series data set for the given stock symbol and period
 get_financial_info <- function(symbol, days_to_plot){
   start_date = Sys.Date() - strtoi(days_to_plot)
   out = get.hist.quote(symbol, start = start_date, end = Sys.Date(), retclass = "ts")
   out = as.data.frame(out) %>% mutate(Date = seq(1, dim(out)[1]))
+  out
+}
+#Saves the output of get_items_and_prices in a csv file for later use 
+save_nice_item_data <- function(item_data){
+  out = subset(item_data, select = -c(results.app_icon)) 
+  colnames(out) <- c("Item name", "Number for sale", "Price of item (cents)", "Price of item (conventional format)", "Name of Game")
+  write.csv(out, file = "Item_data_for_Game.csv")
+}
+
+#Saves the output of get_financial_info in a csv file for later use 
+save_nice_stock_data <- function(stock_data, days = 365){
+  days = seq(Sys.Date() - 364, Sys.Date(), by="days")
+  mutate(stock_data, Date = days)
+  out = na.omit(stock_data)
   out
 }
